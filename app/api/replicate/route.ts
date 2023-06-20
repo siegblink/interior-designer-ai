f28bc6d15ba5a934a1372a1aeded11ce20ec1d95
Siegfred Balona <siegblink@gmail.com>
2023-06-20 13:33:21 +0800

    feat: update replicate api endpoint

--- app/api/replicate/route.ts
+++ app/api/replicate/route.ts
@@ -5,6 +5,10 @@ export async function POST(request: Request) {
   // 1. Get request data (in JSON format) from the client
   const req = await request.json();
 
+  const image = req.image;
+  const theme = req.theme;
+  const room = req.room;
+
   // 2. Initialize the replicate object with our Replicate API token
   const replicate = new Replicate({
     auth: process.env.REPLICATE_API_TOKEN as string,
@@ -12,11 +16,13 @@ export async function POST(request: Request) {
 
   // 3. Set the model that we're about to run
   const model =
-    'cjwbw/rembg:fb8af171cfa1616ddcf1242c093f9c46bcada5ad4cf6f2fbe8b81b330ec5c003';
+    'jagilley/controlnet-hough:854e8727697a057c525cdb45ab037f64ecca770a1769cc52287c2e56472a247b';
 
   // 4. Set the image which is the image we uploaded from the client
   const input = {
-    image: req.image,
+    image,
+    prompt: `A ${theme} ${room} Editorial Style Photo, Symmetry, Straight On, Modern Living Room, Large Window, Leather, Glass, Metal, Wood Paneling, Neutral Palette, Ikea, Natural Light, Apartment, Afternoon, Serene, Contemporary, 4k`,
+    a_prompt: `best quality, extremely detailed, photo from Pinterest, interior, cinematic photo, ultra-detailed, ultra-realistic, award-winning`,
   };
 
   // 5. Run the Replicate's model (to remove background) and get the output image
