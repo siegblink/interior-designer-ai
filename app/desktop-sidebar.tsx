7bca1bb858eb006dffdf1069ad59c3f00b5782d0
Siegfred Balona <siegblink@gmail.com>
2023-07-02 15:42:34 +0800

    feat: add desktop sidebar component

---
+++ app/desktop-sidebar.tsx
@@ -0,0 +1,48 @@
+import { usePathname } from "next/navigation";
+import { navigation } from "@/common";
+import { classNames } from "@/utils";
+
+export function DesktopSidebar() {
+  const pathName = usePathname();
+
+  return (
+    <aside className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+      {/* Sidebar component, swap this element with another sidebar if you like */}
+      <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6">
+        <div className="flex h-16 shrink-0 items-center">
+          <h1 className="bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 bg-clip-text text-xl font-semibold text-transparent">
+            Interior Designer
+          </h1>
+        </div>
+
+        <nav className="flex flex-1 flex-col">
+          <ul role="list" className="flex flex-1 flex-col gap-y-7">
+            <li>
+              <ul role="list" className="-mx-2 space-y-1">
+                {navigation.map((item) => (
+                  <li key={item.name}>
+                    <a
+                      href={item.href}
+                      className={classNames(
+                        pathName === item.href
+                          ? "bg-gray-800 text-white"
+                          : "text-gray-400 hover:bg-gray-800 hover:text-white",
+                        "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 transition-all duration-300"
+                      )}
+                    >
+                      <item.icon
+                        className="h-6 w-6 shrink-0"
+                        aria-hidden="true"
+                      />
+                      {item.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </aside>
+  );
+}
