f17ae0fa67639d5170b4589611f58fe62129ee44
Siegfred Balona <siegblink@gmail.com>
2023-07-06 00:51:06 +0800

    feat: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,40 +1,42 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
-import { Sidebar } from "./sidebar";
+import './globals.css';
+import { Analytics } from '@vercel/analytics/react';
+import { Inter } from 'next/font/google';
+import { Sidebar } from './sidebar';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Interior Designer AI",
-  description: "Upload a sample room photo and get a design back in seconds.",
-  robots: "index, follow",
+  title: 'Interior Designer AI',
+  description: 'Upload a sample room photo and get a design back in seconds.',
+  robots: 'index, follow',
   openGraph: {
-    title: "Interior Designer AI",
-    description: "Upload a sample room photo and get a design back in seconds.",
-    url: "https://interior-designer-ai.vercel.app/",
-    locale: "en_US",
-    type: "website",
+    title: 'Interior Designer AI',
+    description: 'Upload a sample room photo and get a design back in seconds.',
+    url: 'https://interior-designer-ai.vercel.app/',
+    locale: 'en_US',
+    type: 'website',
     images: [
       {
-        url: "https://interior-designer-ai.vercel.app/app-screenshot.png",
+        url: 'https://interior-designer-ai.vercel.app/app-screenshot.png',
         width: 1200,
         height: 630,
-        alt: "Screenshot of the Interior Designer AI app",
+        alt: 'Screenshot of the Interior Designer AI app',
       },
     ],
   },
 };
 
-export default function RootLayout({
-  children,
-}: {
+type RootLayoutProps = {
   children: React.ReactNode;
-}) {
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>
         <Sidebar />
         {children}
+        <Analytics />
       </body>
     </html>
   );
